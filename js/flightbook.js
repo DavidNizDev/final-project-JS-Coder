3325bfe29a5f03b21532ec542238be134abe5885
DavidNIz <dalos58.arg@gmail.com>
2022-07-28 13:37:47 -0300

    ultimas modificaciones

--- js/flightbook.js
+++ js/flightbook.js
@@ -1,4 +1,7 @@
-/* let newList = []; */
+
+
+
+
 
 function dale() {
     const flSubmit = document.getElementById("flSubmit"); // boton submit
@@ -15,19 +18,29 @@ function dale() {
 
     let datosStorage = JSON.parse(localStorage.getItem("listaForm"));
 
-    function agregarVuelo(vuelo) {
-        let fila = document.createElement("tr");
-        let cols = document.createElement("td");
-        let contenido = document.createTextNode(vuelo);
-        cols.appendChild(contenido);
-        fila.appendChild(cols);
-        pintarDatos.appendChild(fila);
-    }
-
+    /*     function agregarVuelo(vuelo) {
+            let fila = document.createElement("tr");
+            let cols = document.createElement("td");
+            let contenido = document.createTextNode(vuelo);
+            pintarDatos.appendChild(fila);
+            fila.appendChild(cols);
+            cols.appendChild(contenido);
+        }
+     */
     flSubmit.addEventListener("click", () => {
         if ((date.value != "") && (depTime.value != "") && (arrivTime.value != "") && (itinerary.value != "") && (typeCraft.value != "") && (registration.value != "") && hp.value != "" && (flightTime.value != "") && (landings.value != "")) {
-            agregarVuelo(date.value, depTime.value, arrivTime.value, itinerary.value, typeCraft.value, registration.value, hp.value, flightTime.value, landings.value);
-
+            /*             agregarVuelo(date.value, depTime.value, arrivTime.value, itinerary.value, typeCraft.value, registration.value, hp.value, flightTime.value, landings.value);
+             */
+            /*             agregarVuelo(date.value);
+                        agregarVuelo(depTime.value);
+                        agregarVuelo(arrivTime.value);
+                        agregarVuelo(itinerary.value);
+                        agregarVuelo(typeCraft.value);
+                        agregarVuelo(registration.value);
+                        agregarVuelo(hp.value);
+                        agregarVuelo(flightTime.value);
+                        agregarVuelo(landings.value);
+             */
             datosStorage.push({
                 date: date.value,
                 depTime: depTime.value,
@@ -40,30 +53,7 @@ function dale() {
                 landings: landings.value
             });
 
-            localStorage.setItem("listaForm", JSON.stringify(datosStorage));
-
-            /* date.value = "";
-            depTime.value = "";
-            arrivTime.value = "";
-            itinerary.value = "";
-            typeCraft.value = "";
-            registration.value = "";
-            hp.value = "";
-            flightTime.value = "";
-            landings.value = ""; */
-
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Field/s missing',
-                text: 'Fill every single field to save your flight!',
-            });
-        }
-
-    })
-
-    if (datosStorage != null) {
-        datosStorage.forEach(e => {
+            let fila = document.createElement("tr");
             let eLfecha = document.createElement("td");
             let eLdespegue = document.createElement("td");
             let eLarrivo = document.createElement("td");
@@ -73,7 +63,70 @@ function dale() {
             let eLcaballos = document.createElement("td");
             let eLtiempo = document.createElement("td");
             let eLatterizajes = document.createElement("td");
+
+            eLfecha.textContent += `${date.value}`;
+            eLdespegue.textContent += `${depTime.value}`;
+            eLarrivo.textContent += `${arrivTime.value}`;
+            eLiti.textContent += `${itinerary.value}`;
+            eLavion.textContent += `${typeCraft.value}`;
+            eLmatricula.textContent += `${registration.value}`;
+            eLcaballos.textContent += `${hp.value}`;
+            eLtiempo.textContent += `${flightTime.value}`;
+            eLatterizajes.textContent += `${landings.value}`;
+
+            pintarDatos.appendChild(fila);
+            fila.appendChild(eLfecha);
+            fila.appendChild(eLdespegue);
+            fila.appendChild(eLarrivo);
+            fila.appendChild(eLiti);
+            fila.appendChild(eLavion);
+            fila.appendChild(eLmatricula);
+            fila.appendChild(eLcaballos);
+            fila.appendChild(eLtiempo);
+            fila.appendChild(eLatterizajes);
+
+
+            localStorage.setItem("listaForm", JSON.stringify(datosStorage));
+
+            date.value = "";
+            depTime.value = "";
+            arrivTime.value = "";
+            itinerary.value = "";
+            typeCraft.value = "";
+            registration.value = "";
+            hp.value = "";
+            flightTime.value = "";
+            landings.value = "";
+
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Your flight has been logged.',
+                showConfirmButton: false,
+                timer: 1500
+            })
+
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Field/s missing',
+                text: 'Fill every single field to save your flight!',
+            });
+        }
+    })
+
+    if (datosStorage != null) {
+        datosStorage.forEach(e => {
             let fila = document.createElement("tr");
+            let eLfecha = document.createElement("td");
+            let eLdespegue = document.createElement("td");
+            let eLarrivo = document.createElement("td");
+            let eLiti = document.createElement("td");
+            let eLavion = document.createElement("td");
+            let eLmatricula = document.createElement("td");
+            let eLcaballos = document.createElement("td");
+            let eLtiempo = document.createElement("td");
+            let eLatterizajes = document.createElement("td");
 
             eLfecha.textContent += `${e.date}`;
             eLdespegue.textContent += `${e.depTime}`;
@@ -104,110 +157,40 @@ function dale() {
 }
 dale();
 
-
-/* const flSubmit = document.getElementById("flSubmit"); // boton submit
-const pintarDatos = document.getElementById("pintarDatos"); // tbody
-let newList = [];
-
-flSubmit.addEventListener("click", () => {
-    let date = document.getElementById("date");
-    let depTime = document.getElementById("depTime");
-    let arrivTime = document.getElementById("arrivTime");
-    let itinerary = document.getElementById("itinerary");
-    let typeCraft = document.getElementById("typeCraft");
-    let registration = document.getElementById("registration");
-    let hp = document.getElementById("hp");
-    let flightTime = document.getElementById("flightTime");
-    let landings = document.getElementById("landings");
-
-    newList.push({
-        date: date.value,
-        depTime: depTime.value,
-        arrivTime: arrivTime.value,
-        itinerary: itinerary.value,
-        typeCraft: typeCraft.value,
-        registration: registration.value,
-        hp: hp.value,
-        flightTime: flightTime.value,
-        landings: landings.value
-    });
-
-    if (date.value == "" || depTime.value == "" || arrivTime.value == "" || itinerary.value == "" || typeCraft.value == "" || registration.value == "" || hp.value == "" || flightTime.value == "" || landings.value == "") {
-        Swal.fire({
-            icon: 'error',
-            title: 'Field/s missing',
-            text: 'Fill every single field to save your flight!',
-        });
-    } else {
-        localStorage.setItem("listaForm", JSON.stringify(newList));
-        let datosStorage = JSON.parse(localStorage.getItem("listaForm"));
-        console.log(datosStorage);
-        datosStorage.forEach(e => {
-            let eLfecha = document.createElement("td");
-            let eLdespegue = document.createElement("td");
-            let eLarrivo = document.createElement("td");
-            let eLiti = document.createElement("td");
-            let eLavion = document.createElement("td");
-            let eLmatricula = document.createElement("td");
-            let eLcaballos = document.createElement("td");
-            let eLtiempo = document.createElement("td");
-            let eLatterizajes = document.createElement("td");
-            let fila = document.createElement("tr");
-
-            eLfecha.textContent += `${e.date}`;
-            eLdespegue.textContent += `${e.depTime}`;
-            eLarrivo.textContent += `${e.arrivTime}`;
-            eLiti.textContent += `${e.itinerary}`;
-            eLavion.textContent += `${e.typeCraft}`;
-            eLmatricula.textContent += `${e.registration}`;
-            eLcaballos.textContent += `${e.hp}`;
-            eLtiempo.textContent += `${e.flightTime}`;
-            eLatterizajes.textContent += `${e.landings}`;
-
-            pintarDatos.appendChild(fila);
-            fila.appendChild(eLfecha);
-            fila.appendChild(eLdespegue);
-            fila.appendChild(eLarrivo);
-            fila.appendChild(eLiti);
-            fila.appendChild(eLavion);
-            fila.appendChild(eLmatricula);
-            fila.appendChild(eLcaballos);
-            fila.appendChild(eLtiempo);
-            fila.appendChild(eLatterizajes);
-        })
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Your flight has been logged.',
-            showConfirmButton: false,
-            timer: 1500
-        });
-        console.log();
-    }
-})
-*/
 const testLogin = document.getElementById("testLogin");
 
 testLogin.addEventListener("click", () => {
     Swal.fire({
         title: 'Register Form',
-        html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
-        <input type="password" id="password" class="swal2-input" placeholder="Password">`,
+        html: `<input type="email" id="email" class="swal2-input" placeholder="Email">
+        <input type="password" id="password" class="swal2-input" placeholder="Password">
+        <br></br>
+        <h6>You will receive a welcoming email :)</h6>`,
         confirmButtonText: 'Sign in',
         focusConfirm: false,
         preConfirm: () => {
-            const login = Swal.getPopup().querySelector('#login').value
+            const email = Swal.getPopup().querySelector('#email').value
             const password = Swal.getPopup().querySelector('#password').value
-            if (!login || !password) {
+            if (!email || !password) {
                 Swal.showValidationMessage(`Please enter email and password`)
+            } else {
+                Swal.fire({
+                    title: 'Welcome to Digital Flightbook!',
+                    text: `All of your data saved in this site, will be stored in your browser LocalStorage.`,
+                    imageUrl: 'https://media.istockphoto.com/photos/low-wing-airplane-picture-id1029268724?k=20&m=1029268724&s=612x612&w=0&h=FzPL3t8HSZKrf1H--uB5J0HMtokegqhgLwVCaklLq2Y=',
+                    imageWidth: 400,
+                    imageHeight: 250,
+                    imageAlt: 'Custom image',
+                })
             }
-            return { login: login, password: password }
+            return { email: email, password: password }
         }
     }).then((result) => {
-        Swal.fire(`
-          Login: ${result.value.login}
-          Password: ${result.value.password}
-        `.trim())
+
     })
 })
 
+/* Swal.fire(`
+  Email: ${result.value.email}
+  Password: ${result.value.password}
+`.trim()) */
